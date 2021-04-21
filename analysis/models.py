7091c65eaafb326053a6dc1a446544f87beaa88e
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-21 09:58:13 +0300

    additional changes

--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Problem(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, Blank=True)
+    part_1 = models.TextField(null=True, Blank=True)
+    part_2 = models.TextField(null=True, Blank=True)
+    part_3 = models.TextField(null=True, Blank=True)
+    cause_1 = models.TextField(null=True, Blank=True)
+    cause_2 = models.TextField(null=True, Blank=True)
+    cause_3 = models.TextField(null=True, Blank=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
+
+class Solution(models.Model):
+    solution_1 = models.TextField(null=True, Blank=True)
+    solution_2 = models.TextField(null=True, Blank=True)
+    solution_3 = models.TextField(null=True, Blank=True)
+    probability = models.FloatField(null=True, blank=True)
+    plan = models.TextField(null=True, Blank=True)
+
+class Reflection(models.Model):
+    result = models.TextField(null=True, Blank=True)
+    solution_pattern = models.TextField(null=True, blank=True)
+    principles = models.TextField(null=True, blank=True)
+
+
+
