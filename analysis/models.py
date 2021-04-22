4b471613737a6fd79f332c86856b3d97df8455ab
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-22 21:37:41 +0300

    additional changes

--- analysis/models.py
+++ analysis/models.py
@@ -1,32 +1,19 @@
 from django.db import models
 
 
-class Goal(models.Model):
-    title = models.CharField(max_length=100, primary_key=True)
-    description = models.TextField(null=True, Blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
-
 class Problem(models.Model):
-    problem = models.ForeignKey(Goal, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, primary_key=True)
-    description = models.TextField(null=True, Blank=True)
-    parts = models.TextField(null=True, Blank=True)
-    causes = models.TextField(null=True, Blank=True)
-    description = models.TextField(null=True, Blank=True)
+    description = models.TextField(null=True, blank=True)
+    parts = models.TextField(null=True, blank=True)
+    causes = models.TextField(null=True, blank=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True, editable=False)
 
 class Solution(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    research = models.TextField(null=True, Blank=True)
-    solutions = models.TextField(null=True, Blank=True)
-    probability = models.FloatField(null=True, blank=True)
-    plan = models.TextField(null=True, Blank=True)
+    research = models.TextField(null=True, blank=True)
+    solutions = models.TextField(null=True, blank=True)
+    plan = models.TextField(null=True, blank=True)
 
-class Reflection(models.Model):
-    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    result = models.TextField(null=True, Blank=True)
-    problem_pattern = models.TextField(null=True, Blank=True)
-    solution_pattern = models.TextField(null=True, blank=True)
-    principles = models.TextField(null=True, blank=True)
 
 
 
--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Problem
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('analysis/index.html')
+    prb = Problem.objects.get_queryset()
+    context = {'prb': prb}
+
+    return HttpResponse(template.render(context, request))
+
