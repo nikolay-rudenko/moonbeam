e8820a8aa9d124151e296f695623c003dd2549f3
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-25 17:19:05 +0300

    additional changes

--- analysis/views.py
+++ analysis/views.py
@@ -4,8 +4,8 @@ from .models import Problem, Solution
 
 def index(request):
     template = loader.get_template('analysis/index.html')
-    prb = Problem.objects.all().order_by('published')
-    slt = Solution.objects.get_queryset().order_by('id')
+    prb = Problem.objects.all()
+    slt = Solution.objects.all()
     context = {'prb': prb, 'slt': slt}
 
     return HttpResponse(template.render(context, request))
