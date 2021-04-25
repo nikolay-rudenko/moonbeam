7da1694c155c0bb47864530949161ddf0bf53516
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-25 18:55:25 +0300

    additional changes

--- analysis/admin.py
+++ analysis/admin.py
@@ -4,14 +4,14 @@ from .models import Problem, Solution
 
 
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'parts', 'causes', 'published')
-    list_display_links = ('title', 'description')
+    list_display = ('title', 'risks', 'description','parts', 'causes', 'published')
+    list_display_links = ('title', 'risks', 'description')
     search_fields = ('title', 'description',)
 
 
 class SolutionAdmin(admin.ModelAdmin):
-    list_display = ('problem', 'research', 'solutions', 'plan', 'test')
-    list_display_links = ('problem','research', 'solutions')
+    list_display = ('id', 'problem', 'research', 'solutions', 'resources', 'plan', 'test')
+    list_display_links = ('problem','research', 'solutions', 'resources')
     search_fields =('problem__title', 'research', 'solutions')
 
 
--- analysis/models.py
+++ analysis/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Problem(models.Model):
     title = models.CharField(max_length=100, primary_key=True)
+    risks = models.TextField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     parts = models.TextField(null=True, blank=True)
     causes = models.TextField(null=True, blank=True)
@@ -17,6 +18,7 @@ class Solution(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     research = models.TextField(null=True, blank=True)
     solutions = models.TextField(null=True, blank=True)
+    resources = models.TextField(null=True, blank=True)
     plan = models.TextField(null=True, blank=True)
     test = models.TextField(null=True, blank=True)
 
