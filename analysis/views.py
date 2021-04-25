e202c76e97491d1a1056ac818b05afc2c4ec58dd
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-25 10:35:36 +0300

    additional changes

--- analysis/views.py
+++ analysis/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Problem
+from .models import Problem, Solution
 
 def index(request):
     template = loader.get_template('analysis/index.html')
     prb = Problem.objects.get_queryset()
-    context = {'prb': prb}
+    slt = Solution.objects.get_queryset()
+    context = {'prb': prb, 'slt': slt}
 
     return HttpResponse(template.render(context, request))
 
