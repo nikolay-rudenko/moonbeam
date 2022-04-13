0727fa362ff6afc368c2dd1e0358afecc5753f0a
Nikolay Rudenko <nikolayrudenko1@gmail.com>
2022-04-13 07:26:45 +0300

    added btn update on home page added time zone

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from analysis.views import index, saved, delete, ProblemCreate, SolutionCreate
+from analysis.views import index, saved, delete, UpdateProblem, ProblemCreate, SolutionCreate
 
 urlpatterns = [
     path('addpr/', ProblemCreate.as_view(), name='addpr'),
+    path('edit/<pk>', UpdateProblem.as_view(), name='update_problem'),
     path('addsl/', SolutionCreate.as_view(), name='addsl'),
     path('saved/', saved, name='saved'),
     path('delete/<problem_id>', delete, name='delete'),
