d6151169d45613c5ae1fc41936a5148b36b23032
Nikolay Rudenko <nikolayrudenko1@gmail.com>
2022-04-20 13:58:08 +0300

    added comments to analysis/view.py

--- analysis/views.py
+++ analysis/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponseRedirect
 from .forms import ProblemForm, SolutionForm
 from django.shortcuts import render
 
-
+# starting page
 def index(request):
     template = loader.get_template('analysis/index.html')
     prb = Problem.objects.select_related()
